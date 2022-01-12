13b1bf80363f7d4beefdae850129bdb2b8c92d2b
Luke Dowell <lukedowell@gmail.com>
2022-01-12 03:08:51 -0600

    First pass of chord symbol validation logic

--- web/src/music/Music.test.tsx
+++ web/src/music/Music.test.tsx
@@ -1,14 +1,27 @@
-import { Chord, isValidVoicing } from './Music'
+import {Chord, isValidVoicing, lowerNote, Note} from './Music'
 
 describe("Chord Voicings", () => {
   test.each([
-    [["C1", "E1", "G1"], { root: "C", quality: "Major" } as Chord],
-    [["C2", "E2", "G2", "C3"], { root: "C", quality: "Major" } as Chord],
-    [["C1", "E5", "F5", "G5", "E6"], { root: "C", quality: "Major" } as Chord],
-    [["C1", "D#1", "G1"], { root: "C", quality: "Minor" } as Chord],
-    [["C1", "D#1", "G1", "C2"], { root: "C", quality: "Minor" } as Chord],
+    [["C1", "E1", "G1"] as Note[], { root: "C", quality: "Major" } as Chord],
+    [["C2", "E2", "G2", "C3"] as Note[], { root: "C", quality: "Major" } as Chord],
+    [["C1", "E5", "F5", "G5", "E6"] as Note[], { root: "C", quality: "Major" } as Chord],
+    [["C1", "D#1", "G1"] as Note[], { root: "C", quality: "Minor" } as Chord],
+    [["C1", "D#1", "G1", "C2"] as Note[], { root: "C", quality: "Minor" } as Chord],
   ])(
     '%s should be a valid voicing of %s',
-    (keys: string[], chord: Chord) => expect(isValidVoicing(chord, keys)).toBe(true)
+    (keys: Note[], chord: Chord) => expect(isValidVoicing(chord, keys)).toBe(true)
+  )
+})
+
+describe("Music Note Utilities", () => {
+  test.each([
+    ["A0", 'C#3'] as Note[],
+    ["G#1", 'G#7'] as Note[],
+    ["E4", 'F4'] as Note[],
+    ["C1", 'C2'] as Note[],
+    ["C1", 'C1'] as Note[],
+  ])(
+    `%s should be lower than %s`,
+    (lower: Note, higher: Note) => expect(lowerNote(lower, higher)).toBe(lower)
   )
 })
--- web/src/music/Music.tsx
+++ web/src/music/Music.tsx
@@ -8,7 +8,7 @@ export type Note = "A0" | "A#0" | "B0" |
   "C7" | "C#7" | "D7" | "D#7" | "E7" | "F7" | "F#7" | "G7" | "G#7" | "A7" | "A#7" | "B7" |
   "C8"
 
-export const KEYBOARD_LAYOUT = [
+export const KEYBOARD: Note[] = [
   "A0", "A#0", "B0",
   "C1", "C#1", "D1", "D#1", "E1", "F1", "F#1", "G1", "G#1", "A1", "A#1", "B1",
   "C2", "C#2", "D2", "D#2", "E2", "F2", "F#2", "G2", "G#2", "A2", "A#2", "B2",
@@ -44,29 +44,77 @@ export const MIDI_NOTE_FLAG = {
 }
 
 export class MIDIPiano {
-  private activeKeys: Array<string> = Array()
-  private listeners: Array<(activeKeys: Array<string>, event: WebMidi.MIDIMessageEvent) => any> = Array()
+  private activeNotes: Array<string> = Array()
+  private listeners: Array<(activeNotes: Array<string>, event: WebMidi.MIDIMessageEvent) => any> = Array()
 
   constructor(midiInput: WebMidi.MIDIInput) {
     midiInput.addEventListener(
       "midimessage",
       (e: WebMidi.MIDIMessageEvent) => {
-        const note = KEYBOARD_LAYOUT[e.data[1] - 21]
+        const note = KEYBOARD[e.data[1] - 21]
         if (e.data[0] === MIDI_NOTE_FLAG.KEY_DOWN) {
-          this.activeKeys.push(note)
+          this.activeNotes.push(note)
         } else if (e.data[0] === MIDI_NOTE_FLAG.KEY_UP) {
-          this.activeKeys = this.activeKeys.filter((i) => i !== note)
+          this.activeNotes = this.activeNotes.filter((i) => i !== note)
         }
-        this.listeners.forEach((c) => c.call(c, this.activeKeys, e))
+        this.listeners.forEach((c) => c.call(c, this.activeNotes, e))
       }
     )
   }
 
-  addListener(callback: (activeKeys: Array<string>, event: WebMidi.MIDIMessageEvent) => any) {
+  addListener(callback: (activeNotes: Array<string>, event: WebMidi.MIDIMessageEvent) => any) {
     this.listeners.push(callback)
   }
 }
 
-export const isValidVoicing = (chord: Chord, activeKeys: Array<string>) => {
-  return false
+export const lowerNote = (a: Note, b: Note) => {
+  if (a === b) {
+    return a
+  }
+
+  const aOctave = Number.parseInt(a.replace("#", "").charAt(1))
+  const bOctave = Number.parseInt(b.replace("#", "").charAt(1))
+
+  if (aOctave < bOctave) return a
+  else if (bOctave < aOctave) return b
+  else {
+    const aNoteValue = a.charCodeAt(0)
+    const bNoteValue = b.charCodeAt(0)
+
+    if (aNoteValue < bNoteValue) return a
+    else return b
+  }
+}
+
+export const noteOctave = (n: Note): number => Number.parseInt(n.charAt(n.length - 1))
+
+export const isValidVoicing = (chord: Chord, activeNotes: Array<Note>): boolean => {
+  const semitones = Array<number>()
+  switch(chord.quality) {
+    case "Diminished":
+      semitones.push(3, 3)
+      break;
+    case "Minor":
+      semitones.push(3, 4)
+      break;
+    case "Major":
+      semitones.push(4, 3)
+      break;
+    case "Augmented":
+      semitones.push(4, 4)
+      break;
+  }
+
+  const lowestRootNote = activeNotes.filter((note) => note.includes(chord.root)).reduce(lowerNote)
+  const transposedActiveNotesWithDuplicates =  activeNotes.map(
+    (n) => `${n.length === 3 ? n.substring(0, 2) : n.charAt(0)}${noteOctave(lowestRootNote)}` as Note
+  )
+  const transposedActiveNotes: Array<Note> = Array.from(new Set<Note>(transposedActiveNotesWithDuplicates))
+
+  const requiredNotes = [lowestRootNote]
+  semitones.forEach((s) => {
+    requiredNotes.push(KEYBOARD[KEYBOARD.indexOf(requiredNotes[requiredNotes.length - 1]) + semitones[requiredNotes.length - 1]])
+  })
+
+  return requiredNotes.every((n) => transposedActiveNotes.includes(n))
 }
