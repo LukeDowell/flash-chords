e937ee3d3ca38650fe17245352fca0dd6f3e8be9
Luke Dowell <lukedowell@gmail.com>
2022-05-17 16:18:10 -0500

    Adds storybook

--- web/src/music/Keys.tsx
+++ web/src/music/Keys.tsx
@@ -16,7 +16,7 @@ export const MAJOR_KEYS = {
   'D♭': {
     notes: ['D♭', 'E♭', 'F', 'G♭', 'A♭', 'B♭', 'C'].map(toNote),
     quality: "major",
-    diatonicChords: ['D♭maj7', 'E♭m7', 'Fm7', 'G♭maj7', 'A♭maj7', 'B♭m7', 'Cdim7'].map(symbolToChord)
+    diatonicChords: ['D♭maj7', 'E♭m7', 'Fm7', 'G♭maj7', 'A♭7', 'B♭m7', 'Cdim7'].map(symbolToChord)
   },
 
   'A♭': {
---
+++ web/src/stories/Measure.stories.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import {Measure} from "../scrollingstaff/Measure";
+import {toNote} from "../music/Note";
+import styled from "@emotion/styled";
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'Measure',
+  component: Measure,
+} as ComponentMeta<typeof Measure>;
+
+const Container = styled('div')({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  width: '300px',
+  height: '80px',
+})
+
+const Template: ComponentStory<typeof Measure> = (args) => <Container>
+  <Measure {...args} />
+</Container>;
+
+export const Treble = Template.bind({});
+Treble.args = {
+  cleff: "treble",
+  notes: ['C4', 'E4', 'G4'].map(toNote)
+}
