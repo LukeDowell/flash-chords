5cac46476121d073b6c39d94caa4ca249dcd2040
Luke Dowell <lukedowell@gmail.com>
2023-01-30 16:11:55 -0600

    Removed old code remnants

--- web/src/pages/home/HomePage.test.tsx
+++
@@ -1,8 +0,0 @@
-import HomePage from './HomePage'
-import {render} from "@testing-library/react";
-
-describe('the home page', () => {
-  it('should render', () => {
-    render(<HomePage/>)
-  })
-})
