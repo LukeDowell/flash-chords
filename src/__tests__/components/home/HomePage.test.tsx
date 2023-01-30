54fe94ae4f5fd51aaa6688d590682bfbee18c8a0
Luke Dowell <lukedowell@gmail.com>
2023-01-30 16:56:35 -0600

    Deleted empty test file

--- src/__tests__/components/home/HomePage.test.tsx
+++
@@ -1,8 +0,0 @@
-import HomePage from '@/components/home/HomePage'
-import {render} from "@testing-library/react";
-
-describe('the home page', () => {
-  it('should render', () => {
-    render(<HomePage/>)
-  })
-})
